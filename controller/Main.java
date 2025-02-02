d0718c06378d519867c75a15d8e230efc64ae6f4
jihea <bictiger3@gamil.com>
2025-02-02 21:57:29 +0900

    Feat : main 수정

--- controller/Main.java
+++ controller/Main.java
@@ -1,29 +1,55 @@
 package controller;
 
 import java.util.Scanner;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        int a,b;
 
-        a = sc.nextInt();
-        b = sc.nextInt();
-        int c = b;
+        int h, m, c;
+        h = sc.nextInt();
+        m = sc.nextInt();
+        c = sc.nextInt();
 
-        // Stream 이용
-//        int[] array = Stream.of(String.valueOf(b).split("")).mapToInt(Integer::parseInt).toArray();
-//        System.out.println(a*array[2]);
-//        System.out.println(a*array[1]);
-//        System.out.println(a*array[0]);
-//        System.out.println(a*b);
+        // 1. 현재시각 시, 분
+        // 2. 요리필요시간 분
 
-        // 나눗셈 연산
-        while(b > 0) {
-            System.out.println(a*(b%10));
-            b /= 10;
+        // 내 문제 풀이
+//        int resM;
+//        int resH;
+//
+//        if((m + c) >= 60) {
+//
+//            resM = (m + c) % 60;
+//            resH = ((m + c) / 60) + h;
+//
+//
+//            if (resH >= 24) {
+//                resH = resH - 24;
+//            }
+//        } else {
+//            resM = m + c;
+//            resH = h;
+//        }
+//
+//        System.out.println(resH + " " + resM);
+//
+
+        // 다른 사람 문제 풀이
+        int resH = h + (m + c) / 60;
+
+        /**
+         *  60을 넘어가는 조건을 안 걸어도 m+c >= 60 이 아니라면
+         *  나머지는 그대로 m+c 가 되기 때문에 조건을 걸 필요가 없었다.
+         * */
+        int resM = (m + c) % 60;
+
+
+        if (resH >= 24) {
+            resH -= 24;
         }
-        System.out.println(a*c);
+
+        System.out.println(resH + " " + resM);
     }
 }
