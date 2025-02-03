b10a979d2fbf609f56f41b67e48d096f5f818425
Kim, Ji-Hea <bictiger3@gmail.com>
2025-02-03 23:36:47 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 816 ms, Memory: 188080 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -21,21 +21,22 @@ public class Main {
             for(int i = 0; i < 2; i++) {
                 array[i] = Integer.parseInt(st.nextToken());
             }
-            
-            // write() 메소드를 통해 출력할 내용 담기 
+
+            // write() 메소드를 통해 출력할 내용 담기
+            // bw 에 쓸 때 String 타입으로 변환해주지 않으면 이상한 문자열이 출력됨 
             bw.write(String.valueOf(array[0] + array[1]));
             bw.write("\n");
-            
+
             n--;
         }
 
-        // inputStream 닫기 
+        // inputStream 닫기
         br.close();
-        
+
         // flush() 메소드를 통해 버퍼를 비워내는 동시에 콘솔에 출력
         bw.flush();
-        
-        // outputStream 닫기 
+
+        // outputStream 닫기
         bw.close();
 
     }
