18ee81c93ea546e827276c70b486a433cd9b0af9
Kim, Ji-Hea <bictiger3@gmail.com>
2025-02-04 23:19:09 +0900

    [Bronze III] Title: 최소, 최대, Time: 464 ms, Memory: 92228 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.OptionalInt;
+import java.util.StringTokenizer;
+
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] x = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < n; i++) {
+            x[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 1. Stream max, min 값 가져오기 => 488ms (Stream 은 너무 느림)
+//        int max = Arrays.stream(x).max().getAsInt();
+//        int min = Arrays.stream(x).min().getAsInt();
+
+        // 2. 배열에서 min, max 값 가져오기
+        int max = x[0];
+        int min = x[0];
+        for(int i = 0; i < n; i++) {
+            if(max < x[i]) {
+                max = x[i];
+            }
+            if(min > x[i]) {
+                min = x[i];
+            }
+        }
+
+        System.out.println(min + " " + max);
+    }
+}
