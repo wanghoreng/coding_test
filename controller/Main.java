8cd7bd70b0b00adfe18a418a664337228a37ac4a
jihea <bictiger3@gamil.com>
2025-02-06 00:26:20 +0900

    Feat : main 수정

--- controller/Main.java
+++ controller/Main.java
@@ -4,36 +4,28 @@ package controller;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] absArr = new int[9];
+        int[] arr = new int[31];
 
-        for(int i = 0; i < 9; i++) {
-            absArr[i] = Integer.parseInt(br.readLine());
+        for(int i =1; i<29; i++) {
+            int pass = Integer.parseInt(br.readLine());
+            arr[pass] = 1;
         }
 
-        int maxTarget = absArr[0];
-        int index = 0;
-
-        // Math.max() 로 최댓값 구하기는 쉽지만 인덱스를 구하기는 어려웠다.
-//        for(int arr: absArr) {
-//            maxTarget = Math.max(maxTarget, Math.abs(arr));
-//        }
-
-        for(int i = 0; i < 9; i++) {
-            if(maxTarget <= absArr[i]) {
-                maxTarget = absArr[i];
-                index = i+1;
+        for(int j=1; j<arr.length; j++) {
+            if(arr[j]!=1) {
+                System.out.println(j);
             }
         }
 
-
-        System.out.println(maxTarget + "\n" + index);
-        br.close();
     }
 }
