e5de2084e0abe74153b8d9d8d59b114211a0b5ea
jihea <bictiger3@gamil.com>
2025-02-06 23:47:38 +0900

    Feat : main 수정

--- controller/Main.java
+++ controller/Main.java
@@ -2,30 +2,16 @@ package controller;
 
 
 import java.io.IOException;
-import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
-import java.util.stream.IntStream;
+import java.io.BufferedReader;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = new int[31];
 
-        for(int i =1; i<29; i++) {
-            int pass = Integer.parseInt(br.readLine());
-            arr[pass] = 1;
-        }
-
-        for(int j=1; j<arr.length; j++) {
-            if(arr[j]!=1) {
-                System.out.println(j);
-            }
-        }
-
+        char a = br.readLine().charAt(0);
+        System.out.println((int)a);
     }
 }
