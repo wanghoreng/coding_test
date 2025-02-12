cef873995ff412386525ebbbd65ba92a9e9f03da
jihea <bictiger3@gamil.com>
2025-02-12 23:34:20 +0900

    Feat : main 수정

--- controller/Main.java
+++ controller/Main.java
@@ -4,14 +4,14 @@ package controller;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.util.StringTokenizer;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String s = br.readLine();
-        System.out.println(s.length());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        System.out.println(st.countTokens());
     }
 }
