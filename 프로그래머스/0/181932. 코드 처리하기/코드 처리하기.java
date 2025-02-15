74d908fe9ce662d3940c9fc073e9a50016bfd94e
Kim, Ji-Hea <bictiger3@gmail.com>
2025-02-15 23:25:42 +0900

    [level 0] Title: 코드 처리하기, Time: 10.08 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -2,29 +2,46 @@ class Solution {
     public String solution(String code) {
         
         int mode = 0; 
-        String result = ""; 
+        
+        // 내 코드 
+        // String result = ""; 
+//         for(int i = 0; i < code.length(); i++) {
+            
+//             if(mode == 0) {
+//                 if(code.charAt(i) == '1') mode = 1;
+//                 else {
+//                     if(i % 2 == 0) {
+//                         result += code.charAt(i); 
+//                     }
+//                 }
+//             } else {
+//                 if(code.charAt(i) == '1') mode = 0;
+//                 else {
+//                     if(i % 2 != 0) {
+//                         result += code.charAt(i); 
+//                     }
+//                 }
+//             }
+            
+            
+//         }
+        
+        // 다른 풀이 
+        StringBuilder result = new StringBuilder(); 
         
         for(int i = 0; i < code.length(); i++) {
             
-            if(mode == 0) {
-                if(code.charAt(i) == '1') mode = 1;
-                else {
-                    if(i % 2 == 0) {
-                        result += code.charAt(i); 
-                    }
-                }
-            } else {
-                if(code.charAt(i) == '1') mode = 0;
-                else {
-                    if(i % 2 != 0) {
-                        result += code.charAt(i); 
-                    }
-                }
+            char c = code.charAt(i);
+            if(c == '1') {
+                mode = mode == 0 ? 1 : 0; 
+                continue; 
             }
             
-            
+            if(i % 2 == mode) {
+                result.append(code.charAt(i)); 
+            }
         }
         
-        return result.equals("") ? "EMPTY" : result;
+        return result.length() == 0 ? "EMPTY" : result.toString();
     }
 }
