951be35de141e1998edd0de5451e955c0a2e8c5c
jihea <bictiger3@gamil.com>
2025-02-17 23:44:17 +0900

    Feat : 2024코테강의_1주차 02 문제풀기

--- dingcorithm/first_week/TwoFinxMaxOccurredAlphabet.java
+++ dingcorithm/first_week/TwoFinxMaxOccurredAlphabet.java
@@ -5,6 +5,8 @@ import java.util.*;
 
 public class TwoFinxMaxOccurredAlphabet {
     public static void main(String[] args) throws IOException {
+        // 그냥 배열을 출력하면 이상한 해시코드로 나오기에
+        // 배열을 출력할 때는 Arrays.toString(배열)을 사용해보자.
         System.out.println(Arrays.toString(findMaxOccurredAlphabet("hello my name is dingcodingco")));
         System.out.println(Arrays.toString(findMaxOccurredAlphabet("we love algorithm")));
         System.out.println(Arrays.toString(findMaxOccurredAlphabet("best of best youtube")));
