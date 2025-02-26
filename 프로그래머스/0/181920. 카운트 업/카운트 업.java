e7af7cd9a46f0d542ca0c1f2a37c810ca176946c
Kim, Ji-Hea <bictiger3@gmail.com>
2025-02-26 23:10:49 +0900

    [level 0] Title: 카운트 업, Time: 4.71 ms, Memory: 87.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        List<Integer> list = new ArrayList<>(); 
+        for(int i = start_num; i <= end_num; i++) {
+            list.add(i); 
+        }
+        
+        
+        return list.stream().mapToInt(i->i).toArray();
+    }
+}
