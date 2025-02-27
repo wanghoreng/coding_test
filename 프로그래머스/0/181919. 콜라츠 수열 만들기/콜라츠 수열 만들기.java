4feb827f9ebba31008ca0ac8f85b9e7ad3eded1d
Kim, Ji-Hea <bictiger3@gmail.com>
2025-02-27 23:37:16 +0900

    Update 콜라츠 수열 만들기.java

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -3,8 +3,8 @@ import java.util.*;
 class Solution {
     public int[] solution(int n) {
         
-        // 내풀이 
-        /*
+        // 내풀이 => 대략 O(N) ? stream 시간복잡도 계산은 어떻게 하지? 젤 느린 시간 (통과 (2.64ms, 76.3MB))
+        
         List<Integer> answer = new ArrayList<>(); 
         answer.add(n); 
         while(n > 1) { // 1과 같거나 작으면 반복문은 끝남 
@@ -17,21 +17,21 @@ class Solution {
             }
         }
         return answer.stream().mapToInt(i->i).toArray();
-        */
+       
         
-        // 다른 풀이 
-        String str = n + ","; 
-        while(n > 1) {
-            n = (n%2 == 0) ? n/2 : n*3+1;
-            str += n + ",";
-        }
+        // 다른 풀이 => 대략 O(2N) ?? 젤 느린 시간(21.95ms, 86.5MB)
+//         String str = n + ","; 
+//         while(n > 1) {
+//             n = (n%2 == 0) ? n/2 : n*3+1;
+//             str += n + ",";
+//         }
         
-        String[] strArr = str.split(",");
-        int[] answer = new int[strArr.length];
-        for(int i = 0; i < answer.length; i++) {
-            answer[i] = Integer.parseInt(strArr[i]); 
-        }
+//         String[] strArr = str.split(",");
+//         int[] answer = new int[strArr.length];
+//         for(int i = 0; i < answer.length; i++) {
+//             answer[i] = Integer.parseInt(strArr[i]); 
+//         }
         
-        return answer; 
+//         return answer; 
     }
 }
