321978c0a5d65ba9ff202bf38d72dbf6ec8de80f
jihea <bictiger3@gamil.com>
2025-03-04 22:11:46 +0900

    Feat : 2024코테강의_2주차 노드 구현 (index 번째 노드 반환, index 로 중간 삽입 및 삭제)

--- dingcorithm/two_week/AppendLinkedList.java
+++ dingcorithm/two_week/AppendLinkedList.java
@@ -88,6 +88,49 @@ public class AppendLinkedList {
         System.out.println();
     }
 
+    // index 번째 노드 반환
+    public ListNode getNode(int index) {
+        ListNode tempNode = this.head;
+         int idx = 0;
+
+         while(idx != index) {
+             tempNode = tempNode.link;
+             idx += 1;
+         }
+
+         return tempNode;
+    }
+
+    // index 번째 원소 추가
+    public static void insertNodeByIndex(AppendLinkedList linkedList, int index, String data) {
+        ListNode newNode = new ListNode(data);
+
+        if(index == 0) {
+            newNode.link = linkedList.head;
+            linkedList.head = newNode;
+        } else {
+            ListNode prevNode = linkedList.getNode(index - 1);
+            newNode.link = prevNode.link;
+            prevNode.link = newNode;
+        }
+
+    }
+
+    // index 번째 원소 삭제
+    public static void deleteNodeByIndex(AppendLinkedList linkedList, int index) {
+
+        ListNode nextNode = linkedList.getNode(index + 1);
+        if(index == 0) {
+            linkedList.head = nextNode;
+        } else {
+            // 이전 노드
+            ListNode prevNode = linkedList.getNode(index - 1);
+            // 현재 인덱스의 다음 노드의 링크를 향하게 하면 된다.
+            prevNode.link = nextNode;
+        }
+    }
+
+
     public static void main(String[] args) {
         AppendLinkedList linkedList = new AppendLinkedList();  // 연결 리스트 생성
         String data = "팰월드";
@@ -99,9 +142,25 @@ public class AppendLinkedList {
 
         linkedList.printList();
 
-        ListNode preNode = linkedList.searchNode("게임 : ");
-        linkedList.insertNode(preNode, data);
+        // node 검색 테스트
+//        ListNode preNode = linkedList.searchNode("게임 : ");
+        // node 중간 삽입 테스트
+//        linkedList.insertNode(preNode, data);
 
-        linkedList.printList();
+        // 모두 출력 테스트
+//        linkedList.printList();
+
+        // index 로 원소 찾기 테스트
+//        System.out.println(linkedList.getNode(1).getData());
+
+        // index 로 중간 추가 테스트
+//        insertNodeByIndex(linkedList,0, data);
+//        linkedList.printList();
+
+        // index 로 중간 삭제 테스트
+//        deleteNodeByIndex(linkedList, 0);
+//        linkedList.printList();
     }
+
+
 }
