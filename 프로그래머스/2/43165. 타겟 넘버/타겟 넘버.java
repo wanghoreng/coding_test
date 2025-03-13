97d7f9d8769056445235fa7ad2a9ca791ba2fd58
Kim, Ji-Hea <bictiger3@gmail.com>
2025-03-13 23:30:29 +0900

    [level 2] Title: 타겟 넘버, Time: 34.14 ms, Memory: 94.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,30 +1,39 @@
-import java.util.*;
+// 1. 배열의 모든 경우의 수를 구한다.
+// 2. 해당 경우의 수를 담는 배열을 만들어 담아준다. 
+// 3. 그 배열에 target 과 같은 수가 있다면 count 를 해준다. 
+
+import java.util.*; 
 
 class Solution {
-    static List<Integer> allSumArray = new ArrayList<>();
-    static int allWayCount;
-
+    
+    // 모든 경우의 수를 담을 배열 
+    static List<Integer> allSumArr = new ArrayList<>(); 
+    // target 을 count 하는 숫자변수 
+    static int targetCnt; 
+    
     public int solution(int[] numbers, int target) {
-        // 1. 배열의 숫자에 대한 모든 경우의 수를 새로운 배열에 저장한다.
-        // 2. 배열의 숫자는 1개당 +,- 를 가질 수 있기에 index 를 올려가며 하나는 + 된 값을 저장하고, 하나는 - 된 값을 저장해야한다.
-        // 3. 새로운 배열에 target 과 같은 숫자가 몇개 있는지 세아린다.
-        getAllSumCountMethod(numbers, target, 0, 0);
-
-        return allWayCount;
+        
+        getTargetCount(numbers, target, 0, 0);                 
+        return targetCnt; 
     }
-
-    private static void getAllSumCountMethod(int[] numbers, int target, int currentIdx, int currentSum) {
-
-        // 재귀 탈출 조건
-        if(numbers.length == currentIdx) {
-            allSumArray.add(currentSum);
-            if(currentSum == target) allWayCount++;
-            return;
+    
+    private static void getTargetCount(int[] numbers, int target, int currentIdx, int currentSum) {
+        
+        // 재귀함수 탈출 조건 (index 가 전달받은 배열의 길이와 같아질 때 탈출!)
+        if(currentIdx == numbers.length) {
+            // 배열의 값을 더한 sum 을 새로 선언한 배열에 담는다. 
+            allSumArr.add(currentSum);
+            // 배열의 값을 더한 sum 이 target 과 같다면 
+            if(currentSum == target) {
+                // targetCount 를 해줌 
+                targetCnt++; 
+            }
+            return; 
         }
-
-        // + 한 값을 저장
-        getAllSumCountMethod(numbers, target,currentIdx + 1, currentSum + numbers[currentIdx]);
-        // - 한 값을 저장
-        getAllSumCountMethod(numbers, target,currentIdx + 1, currentSum - numbers[currentIdx]);
+        
+        // 방법에는 +와 -가 있다. 
+        // 재귀함수 방식으로 배열에 담는다. 
+        getTargetCount(numbers, target, currentIdx + 1, currentSum + numbers[currentIdx]);
+        getTargetCount(numbers, target, currentIdx + 1, currentSum - numbers[currentIdx]);
     }
 }
