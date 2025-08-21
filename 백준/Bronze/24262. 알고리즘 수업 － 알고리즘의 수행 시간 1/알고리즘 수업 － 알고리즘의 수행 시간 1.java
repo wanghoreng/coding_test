4897eb66776cda50d4cab8cc6295bc556a52c9db
Kim, Ji-Hea <bictiger3@gmail.com>
2025-08-21 21:42:41 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 64 ms, Memory: 11428 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,16 @@
+
+public class Main {
+	// MenOfPassion 
+	/*
+	MenOfPassion(A[], n) {	// n 이 입력받은 크기
+	    i = ⌊n / 2⌋; // n을 2로 나눈 값을 i 에 저장 
+	    return A[i]; # 코드1  // A[i]번째 값을 반환 
+	}
+	// 빅오표기법으로 O(1) 
+	 *  */
+	public static void main(String[] args) {
+		System.out.println(1);
+		System.out.println(0);
+	}
+
+}
