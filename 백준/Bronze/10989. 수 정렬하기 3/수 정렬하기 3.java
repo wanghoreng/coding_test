c07f4d298e2860c385b1bc3cf66195a77861e27f
Kim, Ji-Hea <bictiger3@gmail.com>
2025-08-23 22:26:39 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1652 ms, Memory: 481040 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,32 +1,29 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-class Main {
-    public static void main(String[] args) throws IOException{
-        // 카운팅 정렬
+
+// 수 정렬하기 3
+public class Main {
+    // N 은 10,000이 최대
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
         int N = Integer.parseInt(br.readLine());
-        // 1 ~ 10000 까지 수 저장용
-        int[] count = new int[100001];
-
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(br.readLine());
-            // 해당 인덱스의 값을 1 증가
-            count[num]++;
+        // 카운트 정렬
+        int[] count = new int[10001];
+        for(int i = 0; i < N; i++) {
+            int index = Integer.parseInt(br.readLine());
+            count[index]++;
         }
 
-        // 0은 입력범위에서 없으므로 1 부터 시작
-        for (int i = 1; i <= 10000; i++) {
-            // i 값의 개수가 0 이 될 때까지 출력 (빈도수를 의미)
-            while(count[i] > 0) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < 10001; i++) {
+            while(count[i] != 0) {
                 sb.append(i).append("\n");
-                count[i]--;
+                count[i]--; 
             }
         }
 
         System.out.println(sb);
-
     }
 }
